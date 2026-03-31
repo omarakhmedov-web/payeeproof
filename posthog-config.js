ba494b767f9a9d0c49a221ae307c17cb7e9231d9
Omar-LAB <omar.akhmedov@gmail.com>
2026-03-31 21:09:38 +0400

    Update posthog-config.js

--- posthog-config.js
+++ posthog-config.js
@@ -2,9 +2,9 @@ window.PAYEEPROOF_POSTHOG_CONFIG = window.PAYEEPROOF_POSTHOG_CONFIG || {
   token: 'phc_mfpyjHhfB7zpTsR7sTa4QaZVDMUEx4qVTkmp3waK8pA6',
   api_host: 'https://us.i.posthog.com',
   defaults: '2026-01-30',
-  autocapture: true,
+  autocapture: false,
   capture_pageview: false,
-  capture_pageleave: true,
+  capture_pageleave: false,
   person_profiles: 'identified_only',
   persistence: 'localStorage+cookie',
   debug: false
